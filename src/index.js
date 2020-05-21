9326aaaa8eb0b6a1f1c7094715317c3a78934939
irenemonzon <irene.monzonm@gmail.com>
2020-05-21 16:53:00 -0400

    rutas con parametros en node express

--- src/index.js
+++ src/index.js
@@ -1,60 +1,43 @@
 const express = require('express');
-
-const { info, error } = require('./modules/mylog');
-const { countries } = require('countries-list');
+const { countries, languages } = require('countries-list');
+const { info } = require('./modules/mylog');
 
 const app = express();
 
-app.get('/', function(request, response) {
+app.get('/', (request, response) => {
   response.status(200).send('Hello');
 });
 
-app.get('/info', function(request, response) {
+app.get('/info', (request, response) => {
   info('hola info');
   response.send('info nodemon');
 });
 
-app.get('*', function(request, response) {
+app.get('/country', (request, response) => {
+  //console.log('request.query', request.query);
+  response.json(countries[request.query.code]);
+});
+
+app.get('/languages/:lang/', (request, response) => {
+  //console.log('request.params', request.params);
+  const lang = languages[request.params.lang];
+  if (lang) {
+    response.json({
+      status: 'OK',
+      data: lang
+    });
+  } else {
+    response.status(404).json({
+      status: 'NOT FOUND',
+      message: `language ${request.params.lang} not found`
+    });
+  }
+});
+
+app.get('*', (request, response) => {
   response.status(404).send('NOT FOUND');
 });
 
-/*let server = http.createServer(function(request, response) {
-  let parsed = url.parse(request.url);
-  console.log("parsed:", parsed);
-
-  let pathname = parsed.pathname;
-  let query = querystring.parse(parsed.query);
-  console.log("query:", query);
-
-  if (pathname === "/") {
-    response.writeHead(200, { "Content-type": "text/html" });
-    response.write("<html><body><p>Home Page</p></body></html");
-    response.end();
-  } else if (pathname === "/exit") {
-    response.writeHead(200, { "Content-type": "text/html" });
-    response.write("<html><body><p>Bye</p></body></html");
-    response.end();
-  } else if (pathname === "/country") {
-    response.writeHead(200, { "Content-type": "application/json" });
-    response.write(JSON.stringify(countries[query.code]));
-    response.end();
-  } else if (pathname === "/info") {
-    let resul = info(request.url);
-    response.writeHead(200, { "Content-type": "text/html" });
-    response.write(resul);
-    response.end();
-  } else if (pathname === "/error") {
-    let resul = error(request.url);
-    response.writeHead(200, { "Content-type": "text/html" });
-    response.write(resul);
-    response.end();
-  } else {
-    response.writeHead(404, { "Content-type": "text/html" });
-    response.write("<html><body><p>Not Found</p></body></html");
-    response.end();
-  }
-});*/
-
-app.listen(4000, function() {
-  console.log('runnig on 4000');
+app.listen(4000, () => {
+  //console.log('runnig on 4000');
 });
--- src/modules/mylog.js
+++ src/modules/mylog.js
@@ -1,25 +1,11 @@
-/*function info(text) {
-  console.log("INFO:", text);
+const info = text => {
+  console.log('INFO:', text);
   return text;
-}
+};
 
-function error(text) {
-  console.log("ERROR:", text);
+const error = text => {
+  console.log('ERROR:', text);
   return text;
-}
-//exportacion global
-module.exports = { info, error };
-*/
-//exportacion parcial
-
-function info(text) {
-  console.log("INFO:", text);
-  return text;
-}
-
-function error(text) {
-  console.log("ERROR:", text);
-  return text;
-}
+};
 module.exports.info = info;
 module.exports.error = error;
